7d0f685134c0070bd77cf811187ab18759b88e95
Nathan Ashcraft <ngashcraft@gmail.com>
2017-02-17 16:33:16 -0500

    :tada: Transfer learning on GCP

---
+++ finetune/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+  name='transfer_learning',
+  version='1.0',
+  packages=find_packages(),
+  install_requires=['google-cloud-storage'])
