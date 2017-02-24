7a0141d30d5f9978cd8b34253f4742268a78ca2b
Nathan Ashcraft <ngashcraft@gmail.com>
2017-02-24 16:52:59 -0500

    :art: Finish prediction, accuracy, and cost

--- lstm/training/model.py
+++ lstm/training/model.py
@@ -39,8 +39,8 @@ class Model():
         # Model inputs
         # Size: [batches, time, pool_values]
         # TODO(gnashcraft): get input sizes from input config
-        inputs = tf.placeholder(tf.float32, [None, None, None], name='inputs')
-        labels = tf.placeholder(tf.float32, [None, None], name='labels')
+        self._inputs = inputs = tf.placeholder(tf.float32, [None, None, None], name='inputs')
+        labels = tf.placeholder(tf.int32, [None, None], name='labels')
 
         # Trainable variables for linear activation layer
         # TODO(gnashcraft): should probably get number of label options instead of hardcode
@@ -52,15 +52,34 @@ class Model():
         # TODO(gnashcraft): get batch size from input config
         initial_state = cell.zero_state(None, tf.float32)
         outputs, states = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state)
-        # TODO(gnashcraft): Want logits.size = [batch, time, 2]
+        outputs = tf.reshape(outputs, [-1, config.hidden_units])
         logits = tf.nn.xw_plus_b(outputs, weights, bias, name='logits')
 
-        # Output calculations
-        # TODO(gnashcraft):
-        # 1. cost: logits - [batch, time, 2] and labels - [batch, time] and name
-        # 2. accuracy: this and name
-        self._cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
-        self._accuracy = 1.0
+        # Batch predictions
+        # TODO(gnashcraft): should probably get number of label options instead of hardcode
+        # TODO(gnashcraft): get time size
+        self._preds = preds = tf.reshape(tf.nn.softmax(logits), [-1, None, 2], name='predictions')
+
+        # Calculate accuracy
+        missed = tf.not_equal(tf.cast(labels, tf.float32), tf.arg_max(preds, 2), name='missed')
+        self._accuracy = tf.reduce_mean(tf.cast(missed, tf.float32), name='accuracy')
+
+        # Calculate cost
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name='cross_entropy')
+        self._cost = tf.reduce_mean(tf.reduce_sum(cross_entropy, axis=[1]), name='cost')
+
+    def predict(self, sess, inputs):
+        '''Predict a batch of pool_layer activations
+
+        Input:
+            sess: tf.Session; current session
+            inputs: tensor [batch, time, activations]; batch sequential outputs from inception net
+
+        Output:
+            A tensor [batch, time, num_classes] of predictions of each class for each timestep in each batch.
+        '''
+
+        return sess.run(self._preds, feed_dict{self._inputs: inputs})
 
     @property
     def cost(self):
